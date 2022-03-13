9eb85581f30a7ec23eded6bf729125ab0abf9c27
Matej Samolej <matej.samolej1@gmail.com>
2022-03-13 03:31:09 +0100

    Create models.py

---
+++ models.py
@@ -0,0 +1,13 @@
+from django.db import models
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+class Udalost(models.Model):
+    title_en = models.CharField(max_length= 100)
+    title_cz = models.CharField(max_length= 100)
+    desc_en = models.TextField()
+    desc_en = models.TextField()
+    date_start = models.DateField()
+    date_end = models.DateField()
+    author = models.ManyToManyField(Person)
