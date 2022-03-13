acd7c2c065d9925effd349d90e402587334f3736
Matej Samolej <matej.samolej1@gmail.com>
2022-03-13 20:54:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from operator import index
 import pandas as pd
 import requests
 import json
@@ -6,14 +5,14 @@ import os.path
 
 url = 'https://id.muni.cz/simplesaml/module.php/proxystatistics/api.php'
 result = requests.get(url).text
-result = json.loads(result) #because NotImplementedError
-df1 = pd.json_normalize(result) # creating the dataframe
+result = json.loads(result)                                     # because NotImplementedError
+df1 = pd.json_normalize(result)                                 # creating the dataframe
 
 if(os.path.exists('data.csv')): 
     df2 = pd.read_csv('data.csv')
     add = df1[~df1.apply(tuple, 1).isin(df2.apply(tuple,1))]
     had_changed = df2[df2.iloc[:, 0].isin(add.iloc[:, 0])]
-    df2.at[had_changed.index] = add
+    df2.at[had_changed.index] = add                             # update data
     df2.to_csv('data.csv', encoding='utf-8', index=False)
 else:
     df1.to_csv('data.csv', encoding='utf-8', index=False)
