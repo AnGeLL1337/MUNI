cd0b75e4a0326575f0a28c46dceb2d9a939c4404
Matej Samolej <matej.samolej1@gmail.com>
2022-03-13 20:51:46 +0100

    Update

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ df1 = pd.json_normalize(result) # creating the dataframe
 if(os.path.exists('data.csv')): 
     df2 = pd.read_csv('data.csv')
     add = df1[~df1.apply(tuple, 1).isin(df2.apply(tuple,1))]
-    add.to_csv('data.csv', mode='a', encoding='utf-8', header=False, index=False)
+    had_changed = df2[df2.iloc[:, 0].isin(add.iloc[:, 0])]
+    df2.at[had_changed.index] = add
+    df2.to_csv('data.csv', encoding='utf-8', index=False)
 else:
     df1.to_csv('data.csv', encoding='utf-8', index=False)
